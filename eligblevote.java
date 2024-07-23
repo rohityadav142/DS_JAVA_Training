a91ac605461dad802a189869957f91e5760bfea7
rohityadav142 <rohityadav63949083@gmail.com>
2024-07-23 22:39:31 +0530

    oddevenprogram

--- eligblevote.java
+++ eligblevote.java
@@ -1,20 +1,16 @@
 import java.util.*;
 public class eligblevote {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the age");
-        int age =sc.nextInt();
-        if(age>18){
+        int age = sc.nextInt();
+        if (age > 18) {
             System.out.println("you can vote");
-        } else{
+        } else {
             System.out.println(" not eligible");
         }
-        {
-            
-        }
-      
-        }
-        
-    }
-    
+        sc.close();
 
+    }
+
+}
