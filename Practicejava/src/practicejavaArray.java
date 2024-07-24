9525bafb0496bb2a1dc74d26fa8893a3b30fa2ba
rohityadav142 <rohityadav63949083@gmail.com>
2024-07-24 11:57:25 +0530

    array code

---
+++ Practicejava/src/practicejavaArray.java
@@ -0,0 +1,11 @@
+public class practicejavaArray {
+    public static void main(String[] args) {
+        String[]name=new String[5];
+        name=new String[]{ "Rohit " ,"Anuj ","shashi","kamal","Satyam"};
+        for (int i = 0; i < name.length; i++) {
+            System.out.println(name[i] +" ");
+
+        }
+
+        }
+    }
