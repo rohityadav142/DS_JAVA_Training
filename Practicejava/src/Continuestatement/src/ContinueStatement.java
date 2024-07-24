f3415c53743b0bb37841c30b163d0b4dbd3d174c
rohityadav142 <rohityadav63949083@gmail.com>
2024-07-24 23:36:58 +0530

    evenoddnousingcontinuestatement

---
+++ Practicejava/src/Continuestatement/src/ContinueStatement.java
@@ -0,0 +1,12 @@
+public class ContinueStatement {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            if (i%2==0) {
+                System.out.println("even no "+i);
+
+            }else continue;
+
+
+        }
+    }
+}
