5b4a476a612fc84e33d6de5576f08a62fd07de16
rohityadav142 <rohityadav63949083@gmail.com>
2024-07-25 00:26:17 +0530

    additoninArray

---
+++ AddtionArray/src/AddtionArraydemo.java
@@ -0,0 +1,12 @@
+public class AddtionArraydemo {
+    public static void main(String[] args) {
+        int[] array1={1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for (int i = 0; i <= array1.length; i++) {
+
+            sum=sum+i;
+
+        }
+        System.out.println(sum);
+    }
+}
